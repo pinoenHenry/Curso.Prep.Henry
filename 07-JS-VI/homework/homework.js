3fae4ecf9126668c752f79e648063108011293eb
pinoen <pinoen2016@gmail.com>
2022-03-20 21:27:06 -0300

    JSVI others solutions

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,20 +4,20 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
-  return nombre[0].toUpperCase() + nombre.slice(1);
+  return nombre.charAt(0).toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
-  return cb();
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
-  return cb(n1 + n2);
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -51,9 +51,7 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
-  var filtro = array.filter(function(letter){
-    return letter[0] === 'a';
-  })
+  var filtro = array.filter(letter => letter[0] === 'a');
   return filtro;
 }
 
